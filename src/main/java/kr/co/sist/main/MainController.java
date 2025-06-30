31effd175a33ba06e667ba5551edc4fd83aeaca6
Seh-Kim <tpqkd99@naver.com>
2025-06-30 16:14:38 +0900

    김세형 첫 커밋

---
+++ src/main/java/kr/co/sist/main/MainController.java
@@ -0,0 +1,15 @@
+package kr.co.sist.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("loggedIn", false); // or true if authenticated
+		return "ksh/main";
+	}
+}
