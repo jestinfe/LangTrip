c133ec32f447c727eb610d79a5ceac54f8117aab
cseungjae <csj103140@gmail.com>
2025-06-30 16:19:32 +0900

    세형형님뭐하세형?

--- src/main/java/kr/co/sist/main/MainController.java
+++ src/main/java/kr/co/sist/main/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
+//ㅇㅇ
 	@GetMapping("/")
 	public String main(Model model) {
 		model.addAttribute("loggedIn", false); // or true if authenticated
