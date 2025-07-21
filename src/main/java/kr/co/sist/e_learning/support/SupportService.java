7e57be38befd14688fcce070871a01599237ba96
qnlnp <qnlnpious@gmail.com>
2025-07-21 16:26:52 +0900

    지원센터 재커밋~~

--- src/main/java/kr/co/sist/e_learning/support/SupportService.java
+++ src/main/java/kr/co/sist/e_learning/support/SupportService.java
@@ -4,36 +4,109 @@ import java.util.List;
 
 public interface SupportService {
 
-	public boolean add(Object dto);// 항목추가
+	public boolean addNotice(NoticeDTO dto);// 단일 Notice 추가
 
-	public List<NoticeDTO> searchAllNotice(String type);// 전체 NoticeDTO 조회
+	public boolean addFaq(FaqDTO dto);// 단일 Faq 추가
 
-	public List<FaqDTO> searchAllFaq(String type);// 전체 FaqDTO 조회
+	public boolean addFeedback(FeedbackDTO dto);// 단일 Feedback 추가
 
-	public List<FeedbackDTO> searchAllFeedback(String type);// 전체 FeedbackDTO 조회
+	// ---------------------------------------------------------------------
 
-	public NoticeDTO searchOneNotice(String type, String id);// 상세 NoticeDTO 조회
+	public List<NoticeDTO> searchAllNotice();// 전체 Notice 조회
 
-	public FaqDTO searchOneFaq(String type, String id);// 상세 FaqDTO 조회
+	public List<NoticeDTO> searchAllNoticeAdmin(); // 전체 Notice 조회(관리자)
 
-	public FeedbackDTO searchOneFeedback(String type, String id);// 상세 FeedbackDTO 조회
+	public List<FaqDTO> searchAllFaq();// 전체 Faq 조회
 
-	public boolean edit(String id);// 내용수정
+	public List<FeedbackDTO> searchAllFeedback();// 전체 Feedback 조회
+	public List<FeedbackDTO> searchTotalFeedback();// 전체 Feedback 조회
 
-	public boolean editDto(Object dto);
+	// ---------------------------------------------------------------------
 
-	public boolean remove(String id);// 항목삭제
+	public NoticeDTO searchOneNotice(String id);// 상세 Notice 조회
 
-	public boolean removeDto(Object dto);
+	public FaqDTO searchOneFaq(String id);// 상세 FaqDTO 조회
 
-	public boolean increaseHit(String id);// 조회수 증가
+	public FeedbackDTO searchOneFeedback(String id);// 상세 Feedback 조회
 
-	public boolean increaseHitDto(Object dto);
+	// ---------------------------------------------------------------------
 
-	//////////////////////////////////////////////////////////////
+	public boolean editNoticeTitle(String Id, String newTitle);
 
-	public String identifyDtoToCurrentId(Object dto);
+	public boolean editNoticeContent(String Id, String newContent);
 
-//	public String safeIdentifyDtoId(Object dto);
+	public boolean editNoticeFixFlag(String Id, String fixFlag);
+
+	public boolean editNoticeHit(String id);
+
+	public int editNoticeStatusInactive(String id);// delete 대체=비활성
+
+	public int editNoticeStatusActive(String id);// delete 대체=활성
+
+	// -----
+
+	public boolean editFaqTitle(String id, String newTitle);
+
+	public boolean editFaqContent(String id, String newContent);
+
+	public boolean editFaqtype(String id, String type);
+
+	public boolean editFaqHit(String id);
+
+	public boolean editFaqStatus(String id);// delete 대체
+
+	// -----
+
+	public boolean editFeedbackStatus(String id);
+
+	public boolean editFeedbackStep(String id);
+	
+	public boolean editFeedbackRefuse(String id);
+
+	public boolean editFeedbackStepFinished(String id);
+
+	// ---------------------------------------------------------------------
+
+	public boolean respondToFaq(String noticeId, String responseType); // 해당 faq에 대한 피드백
+
+	public boolean saveMarkFaq(String userId, String faqId);// 해당 faq 즐겨찾기 추가
+
+	public boolean saveUnfinishedFeedback(String userId, String feedbackStatus);// 피드백 임시저장
+
+	public boolean notifyFeedbackProgress(String feedbackId, String userId); // 피드백 진척도 이메일 안내
+
+	public boolean suggestBetterKeywords(String rawKeyword); // 지원센터 통합검색 키워드 제안
+
+	public boolean switctSupportCenterMode(String mode);// 사용자 맞춤형 지원센터 모드변경
+
+	// ---------------------------------------------------------------------
+
+	public List<String> findTitlesByKeyword(String keyword);
+
+	public NoticeDTO findOneByTitle(String title);
+
+	public List<NoticeDTO> findNoticesByKeyword(String keyword);
+
+	public List<FaqDTO> searchFaqsByType(String typeId);
+
+	public List<FaqDTO> searchAllFaqDTO();
+
+	public List<FaqtypeDTO> selectActiveFaqTypes();
+
+	public List<FaqDTO> searchTop3Faq();
+
+	public List<FaqDTO> selectFaqByType(String typeId);
+
+	public List<FaqtypeDTO> searchAllFaqtype();
+
+	public List<FeedbackDTO> searchAllNewFeedback();
+
+	public List<FeedbackDTO> searchAllNiceFeedback();
+
+	public int searchMaxFaqHits();
+
+	public int searchMaxFeedbackSends();
+	
+	
 
 }
