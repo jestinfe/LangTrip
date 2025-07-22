dfeacb4a38b23e6c40f00bad2a74931fa7c0d79b
gyumin6349 <shjobo2@naver.com>
2025-07-22 15:00:33 +0900

    심규민

--- src/main/java/kr/co/sist/e_learning/course/CourseServiceImpl.java
+++ src/main/java/kr/co/sist/e_learning/course/CourseServiceImpl.java
@@ -1,26 +1,66 @@
 package kr.co.sist.e_learning.course;
 
-import kr.co.sist.e_learning.pagination.PageResponseDTO;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import jakarta.servlet.http.HttpSession;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
-    @Autowired
-    private CourseMapper courseMapper;
+	@Autowired
+	private CourseMapper cm;
+	
+	
+	@Override
+	public void addCourse(CourseDTO cDTO) {
+		cm.insertCourse(cDTO);
+	}
 
-    @Override
-    public PageResponseDTO<UserCourseListDTO> getAllCourses(Map<String, Object> params) {
-        int page = (int) params.get("page");
-        int pageSize = (int) params.get("pageSize");
 
-        List<UserCourseListDTO> list = courseMapper.selectAllCourses(params);
-        int totalCount = courseMapper.countAllCourses(params);
+	@Override
+	public List<CourseDTO> selectCourse(String userId) {
+		
+		return cm.searchCourseById(userId);
+	}
 
-        return new PageResponseDTO<>(list, totalCount, page, pageSize, 5);
-    }
+
+	@Override
+	public CourseDTO selectCourseData(String courseSeq) {
+		return cm.searchCourseByCourseId(courseSeq);
+	}
+
+
+	@Override
+	public List<CourseDTO> selectCourseByPage(Map<String, Object> param) {
+		
+		
+		return cm.searchCourseByPage(param);
+	}
+
+
+	@Override
+	public int selectCourseCount(String userId) {
+		return cm.searchCourseCount(userId);
+	}
+
+
+	@Override
+	public int modifyCourse(CourseDTO cDTO) {
+		int result = 0;
+		result = cm.updateCourseByCourseSeq(cDTO);
+		return result;
+	}
+
+	@Override
+	public int removeCourse(String courseSeq) {
+	
+		
+		return  cm.deleteCourseByCourseSeq(courseSeq);
+	}
+	
 }
